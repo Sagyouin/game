7537bed66dcea68842c4156417bc855576f1a4b1
Sagyouin <the.holiday.syndrome@gmail.com>
2014-08-10 23:50:40 +0900

    Delete loadBaseObj.js

--- baseJs/obj/loadBaseObj.js
+++
@@ -1,3 +0,0 @@
-var GAME_BASE_OBJ_DIR = "./baseJs/obj";
-document.write("<script src='" + GAME_BASE_OBJ_DIR + "/baseLib.js'></script>");
-document.write("<script src='" + GAME_BASE_OBJ_DIR + "/touch.js'></script>");
